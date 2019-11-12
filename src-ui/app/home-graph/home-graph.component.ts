dc383cc9468cf14e2aa4e5a05cf0a9d60c187c60
Rosie Sun <weijias1@andrew.cmu.edu>
2019-11-12 16:08:18 -0500

    finish viz, need hover effect

--- src-ui/app/home-graph/home-graph.component.ts
+++ src-ui/app/home-graph/home-graph.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Inject } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import "leader-line";
+declare let LeaderLine: any;
 
 @Component({
   selector: "app-home-graph",
@@ -8,5 +10,57 @@ import { Component, OnInit, Inject } from "@angular/core";
 export class HomeGraphComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  addLine(id1, id2) {
+    var v1 = document.getElementById(id1);
+    var v2 = document.getElementById(id2);
+    var line = new LeaderLine(v1, v2, { color: "#eaeaea", size: 2 });
+  }
+
+  ngOnInit() {
+    const pairs_left = [
+      ["app-twitter", "app-data-1"],
+      ["app-twitter", "app-data-11"],
+      ["app-fb", "app-data-2"],
+      ["app-fb", "app-data-3"],
+      ["app-google", "app-data-5"],
+      ["app-google", "app-data-6"],
+      ["app-google", "app-data-7"],
+      ["app-google", "app-data-8"],
+      ["app-youtube", "app-data-9"],
+      ["app-youtube", "app-data-10"],
+      ["app-tiktok", "app-data-12"],
+      ["app-tiktok", "app-data-13"],
+      ["app-tiktok", "app-data-4"],
+      ["app-data-1", "cs-twitter"],
+      ["app-data-2", "cs-fb"],
+      ["app-data-3", "cs-fb"],
+      ["app-data-4", "cs-fb"],
+      ["app-data-5", "cs-google"],
+      ["app-data-6", "cs-google"],
+      ["app-data-7", "cs-google"],
+      ["app-data-8", "cs-google"],
+      ["app-data-9", "cs-google"],
+      ["app-data-10", "cs-google"],
+      ["app-data-11", "cs-tps"],
+      ["app-data-12", "cs-unknown"],
+      ["app-data-13", "cs-unknown"]
+    ];
+    const pairs_right = [];
+    pairs_left.forEach(pair => {
+      // var v1 = document.getElementById(pair[0]);
+      // var v2 = document.getElementById(pair[1]);
+      // var line = new LeaderLine(v1, v2, { color: "#eaeaea", size: 2 });
+      this.addLine(pair[0], pair[1]);
+    });
+    // pairs_right.forEach(pair => {
+    //   this.addLine(pair[0], pair[1]);
+    // });
+  }
+
+  // v1.onmouseenter = function() {
+  //   line.color = "#50abf1";
+  // };
+  // v1.onmouseleave = function() {
+  //   line.color = "#eaeaea";
+  // };
 }
